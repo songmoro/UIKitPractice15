4b2c1be0e9d89fbfb6a59e0ef4c07c3e33830add
songmoro <wlqgkr@naver.com>
2025-07-18 17:01:21 +0900

    item sizing

--- UIKitPractice15/UpDown/UpDownCollectionViewController.swift
+++ UIKitPractice15/UpDown/UpDownCollectionViewController.swift
@@ -19,7 +19,21 @@ class UpDownCollectionViewController: UIViewController, UICollectionViewDelegate
         collectionView.register(UINib(nibName: "UpDownCell", bundle: nil), forCellWithReuseIdentifier: "UpDownCell")
         
         let layout = UICollectionViewFlowLayout()
+        let screen = UIScreen.main.bounds
+        
+        let horizontalSpacing = screen.width * 0.05
+        let itemSpacing = screen.width * 0.05
+        let numberOfItems = 5.0
+        let numberOfInsets = numberOfItems - 1
+        let totalItemSize = (screen.width - (horizontalSpacing + horizontalSpacing) - (itemSpacing * numberOfInsets))
+        let itemSize = totalItemSize / numberOfItems
+        
         layout.scrollDirection = .horizontal
+        layout.minimumInteritemSpacing = itemSpacing
+        layout.itemSize = CGSize(width: itemSize, height: itemSize)
+        layout.sectionInset = UIEdgeInsets(top: 0, left: horizontalSpacing, bottom: 0, right: horizontalSpacing)
+        
+        collectionView.showsHorizontalScrollIndicator = false
         collectionView.collectionViewLayout = layout
     }
     
