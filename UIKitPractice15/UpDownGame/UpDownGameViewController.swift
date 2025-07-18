7304da8beb9c72431d9eae5533ecac6a4ddf5e04
songmoro <wlqgkr@naver.com>
2025-07-18 13:46:18 +0900

    컬러 에셋 추가 | 백그라운드 컬러 설정

---
+++ UIKitPractice15/UpDownGame/UpDownGameViewController.swift
@@ -0,0 +1,19 @@
+//
+//  UpDownGameViewController.swift
+//  UIKitPractice15
+//
+//  Created by 송재훈 on 7/18/25.
+//
+
+import UIKit
+
+class UpDownGameViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureView()
+    }
+    
+    private func configureView() {
+        self.view.backgroundColor = UIColor.lightBlue
+    }
+}
