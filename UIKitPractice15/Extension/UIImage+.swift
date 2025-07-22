a1e953f8226352de07afc2e0b619cd0bec634ced
songmoro <wlqgkr@naver.com>
2025-07-22 16:25:09 +0900

    투두 추가

--- UIKitPractice15/Extension/UIImage+.swift
+++ UIKitPractice15/Extension/UIImage+.swift
@@ -12,3 +12,17 @@ extension UIImage {
         self.init(named: "TravelTalk/\(imageNamed)")
     }
 }
+
+
+// TODO: 채팅창
+/** 채팅창
+ 1. 메시지 보내기
+ 2. 랜덤 응답
+ 3. 날짜 바뀌면 섹션 바꾸기
+ 4. 뷰 진입 시 바텀 포지션
+ */
+
+// TODO: 채팅목록
+/** 채팅목록
+ 1. 검색 기능
+ */
